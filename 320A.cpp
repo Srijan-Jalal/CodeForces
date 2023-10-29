ca9711d2242fff0f3a5eb0d1563be8b46b60426e
Srijan <jalalsrijan@gmail.com>
2023-10-29 09:23:31 +0530

    Solution of Question no: 320A in CodeForces

---
+++ 320A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int i,j,k;
+        string s;
+        bool flag=true;
+        cin>>s;
+        for(i=0;i<s.length();)
+        {
+            if(s[i]=='1' && s[i+1]=='4' && s[i+2]=='4')
+                i+=3;
+            else if(s[i]=='1' && s[i+1]=='4')
+                i+=2;
+            else if(s[i]=='1')
+                i++;
+            else
+            {
+                flag=false;
+                break;
+            }
+        }
+
+        if(flag)
+            cout<<"YES";
+        else
+            cout<<"NO";
+
+        return 0;
+}

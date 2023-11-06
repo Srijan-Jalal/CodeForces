5949cdb15e8eec207cca761193d56dcbec1e3696
Srijan <jalalsrijan@gmail.com>
2023-11-06 21:24:25 +0530

    Solution of Question no: 1426A in CodeForces

---
+++ 1426A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t,n,x;
+    cin>>t;
+    while(t--!=0)
+    {
+        cin>>n>>x;
+        int c = 2,ans = 1;
+        while(c < n)
+        {
+            c += x;
+            ans++;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}

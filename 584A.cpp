8729f43225075bf85ea8b65258812029b255e898
Srijan <jalalsrijan@gmail.com>
2023-11-14 11:50:21 +0530

    Solution of Question no: 584A in CodeForces

---
+++ 584A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long int x,y,n,i,t;
+    cin>>n>>t;
+    if(n==1&&t==10)
+        cout<<"-1\n";
+    else if(n>=2&&t==10)
+    {
+        for(i=1;i<n;i++)
+            cout<<"1";
+        cout<<"0\n";
+    }
+    else
+        for(i=1;i<=n;i++)
+            cout<<t;
+
+    return 0;
+}

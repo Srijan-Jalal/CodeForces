8b027eff4ffad29df36b2839d998a1905ec8d7d4
Srijan <jalalsrijan@gmail.com>
2023-12-06 19:40:41 +0530

    Solution of Question no: 46A in CodeForces

---
+++ 46A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int n, c = 2;
+    cin >> n;
+    cout << c;
+    for (int i = 2; i < n; ++i)
+    {
+        c += i;
+        if (c > n)
+            c -= n;
+        cout << " " << c;
+    }
+    cout<<endl;
+    return 0;
+}
